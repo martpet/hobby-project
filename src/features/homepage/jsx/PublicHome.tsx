0b9b20bfc65ab6d1fd1bd58a1fe5fc1cddb6db84
Martin Petrov <martin_petrov@me.com>
2026-09-02 04:42:12 +0300

    fix: update noscript message to clarify JavaScript requirement for signup

--- src/features/homepage/jsx/PublicHome.tsx
+++ src/features/homepage/jsx/PublicHome.tsx
@@ -8,7 +8,7 @@ export function PublicHome(_props: unknown, { assets }: Context) {
   return (
     <Page>
       <h1>Hobby Project</h1>
-      <noscript>JavaScript is required to log in.</noscript>
+      <noscript>JavaScript is required to signup in.</noscript>
       <LogInButton />
       <p>
         <a href="/signup">Create an account</a>
