1d6420f3044a41753bc18fd637fbef9fa992025e
Martin Petrov <martin_petrov@me.com>
2026-09-02 14:30:44 +0300

    add missing login assets to UnauthorizedPage

--- src/shared/jsx/pages/Unauthorized.tsx
+++ src/shared/jsx/pages/Unauthorized.tsx
@@ -1,11 +1,17 @@
 import { LogInButton } from "@features/sessions/jsx/LogInButton.tsx";
 import { Page } from "@shared/jsx/Page.tsx";
+import { Context } from "@shared/types.ts";
 
 interface UnauthorizedPageProps {
   heading?: string;
 }
 
-export function UnauthorizedPage({ heading }: UnauthorizedPageProps) {
+export function UnauthorizedPage(
+  { heading }: UnauthorizedPageProps,
+  { assets }: Context,
+) {
+  assets.add("login");
+
   const headingUsed = heading || "Unauthorized";
 
   return (
