990d46e681a8faff119b327f679034b631eda8ca
Martin Petrov <martin_petrov@me.com>
2026-09-05 15:36:50 +0300

    remove dot from username field as firefox adds one

--- src/features/account/jsx/DeleteAccountDialog.tsx
+++ src/features/account/jsx/DeleteAccountDialog.tsx
@@ -45,6 +45,7 @@ export function DeleteAccountDialog(
         <button
           command="close"
           commandfor={DELETE_ACCOUNT_DIALOG}
+          autofocus
         >
           Cancel
         </button>
--- src/features/users/const.ts
+++ src/features/users/const.ts
@@ -4,4 +4,4 @@ export const USERNAME_PATTERN_REGEX =
   /^[a-zA-Z0-9][a-zA-Z0-9_\-]{1,28}[a-zA-Z0-9]$/;
 
 export const USERNAME_PATTERN_DESCRIPTION =
-  "3-30 letters, numbers, underscores, or hyphens. Cannot start or end with a symbol.";
+  "3-30 letters, numbers, underscores, or hyphens. Cannot start or end with a symbol";
